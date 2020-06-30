63b6b4b97f65662179fd73dc9d97ce9383dce6eb
luiiccini <lsanchez@lasallistas.org.mx>
2020-06-30 10:24:05 -0500

    entropia

--- var_graficas.R
+++ var_graficas.R
@@ -1,14 +1,12 @@
-
-#######TAREA 1.4
-
-getwd()
 if(!require(quantmod)) install.packages('quantmod')
 library(quantmod)
 library(dplyr)
 library(magrittr)
 library(ggplot2)
 library(lubridate)
+library(arules)
 
+setwd()
 data_env<- new.env()
 symbols<- c("AMXL.MX")
 
@@ -20,7 +18,7 @@ Datos_rendimientos<- apply(Datos, 2, ROC, type="discrete")
 Datos_rendimientos %<>% as.data.frame()
 Datos_rendimientos <- Datos_rendimientos[-1,]
 Datos_rendimientos %<>% as.data.frame()
-names(Datos_rendimientos) <- symbols
+names(Datos_rendimientos) <- 'Rendimiento'
 Datos %<>% as.data.frame() 
 Datos["Fecha"] <- Datos %>% row.names()
 Datos["Año"] <- str_sub(Datos["Fecha"]$Fecha,start=1,end = 4)
@@ -28,9 +26,23 @@ fechas <- Datos$Fecha[c(2:nrow(Datos))]
 Datos_rendimientos['Fecha'] <- fechas
 Datos_rendimientos["Año"] <- str_sub(Datos_rendimientos["Fecha"]$Fecha,start=1,end = 4)
 
+entropia <- function(df){
+  test <- discretize(df$Rendimiento,breaks = 30)
+  test <- as.vector(test)
+  dist <- test %>% table()/length(test)
+  suma <- 0
+  for (i in dist){
+    h <- i*log2(i)
+    suma <- suma + h
+  }
+  return(-suma)
+}
+
 calculo_var <- function(df=Datos_rendimientos,precios = Datos,acciones=100,year){
   df %<>% filter(`Año`==year) 
-  precios %<>% filter(`Año`==year) 
+  precios %<>% filter(`Año`==year)
+  graf <- df %>% select(-c(`Año`,Fecha)) %>% ggplot(aes(x=Rendimiento)) + geom_histogram() +
+    ggtitle(year)
   S <- precios[1,1]*acciones
   media <- df %>% select(-c(`Año`,Fecha)) %>% colMeans()
   test <- df %>% select(-c(`Año`,Fecha)) %>% var()
@@ -40,10 +52,24 @@ calculo_var <- function(df=Datos_rendimientos,precios = Datos,acciones=100,year)
   desv <- (w%*%test%*%wt %>% sqrt())[1,1]
   t <- nrow(df)
   VaR <- qnorm(0.95,media,desv)*S*desv*sqrt(t)
-  return(VaR)
+  ent <- df %>% entropia()
+  return(list('var'=VaR,'hist'=graf,'entropia'=ent))
 }
 
 #Hacer for para calcular var cada año y los histogramas de cada 
-calculo_var(year = '2019')
-write.csv(tabla, file = "C:/Users/bluem/OneDrive/Documentos/An?lisis N?merico/TABLA_TAREA1.csv")
+vars <- c()
+hist <- c()
+entropias <- c()
+for (i in c('2015','2016','2017','2018','2019')){
+  output <- calculo_var(year=i)
+  vars <- c(vars,output$var)
+  hist <- c(hist,output$hist)
+  entropias <- c(entropias,output$entropia)
+}
 
+
+out <- data.frame(Año=c('2015','2016','2017','2018','2019'))
+out['VaR'] <- vars
+out['Entropia'] <- entropias
+
+write.csv(out, file = "Output/var_entropia.csv",row.names = F)
