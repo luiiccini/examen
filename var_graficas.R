824c61825424969abd20d57f19df383ea767ffde
luiiccini <lsanchez@lasallistas.org.mx>
2020-06-30 01:13:34 -0500

    tercer avance

--- var_graficas.R
+++ var_graficas.R
@@ -4,88 +4,46 @@
 getwd()
 if(!require(quantmod)) install.packages('quantmod')
 library(quantmod)
-
+library(dplyr)
+library(magrittr)
+library(ggplot2)
+library(lubridate)
 
 data_env<- new.env()
-symbols<- c("AMXL.MX", "GMEXICOB.MX","ALFAA.MX","GFNORTEO.MX","FEMSAUBD.MX")
+symbols<- c("AMXL.MX")
 
-getSymbols(Symbols=symbols, env=data_env, from="2019-06-05", 
-	to="2020-06-05")
+getSymbols(Symbols=symbols, env=data_env, from="2015-01-01", 
+	to="2019-12-31")
 
 Datos<- do.call(merge, eapply(data_env, Cl))
-View(Datos)
-names(Datos)
-##############################################################################
 Datos_rendimientos<- apply(Datos, 2, ROC, type="discrete")
-Datos_rendimientos<-as.data.frame(Datos_rendimientos[-1,])
-names(Datos_rendimientos)
+Datos_rendimientos %<>% as.data.frame()
+Datos_rendimientos <- Datos_rendimientos[-1,]
+Datos_rendimientos %<>% as.data.frame()
+names(Datos_rendimientos) <- symbols
+Datos %<>% as.data.frame() 
+Datos["Fecha"] <- Datos %>% row.names()
+Datos["Año"] <- str_sub(Datos["Fecha"]$Fecha,start=1,end = 4)
+fechas <- Datos$Fecha[c(2:nrow(Datos))]
+Datos_rendimientos['Fecha'] <- fechas
+Datos_rendimientos["Año"] <- str_sub(Datos_rendimientos["Fecha"]$Fecha,start=1,end = 4)
 
-
-Datos_rendimientos<- Datos_rendimientos[complete.cases(Datos_rendimientos),]
-
-View(Datos_rendimientos)
-
-##############################################################################
-
-	matriz<-data.frame(Variable=double(),x=double(),y0=double(),y=double(),
-	y1=double(),Nx1=double(),xa=double(),Y02=double(),y2=double(),
-	y12=double(),Nx2=double())
-
-
-	particiones<-100
-	series<-10
-	x<-0.01
-	xa<-0.05
-
-
-
-for(i in 1:series){
-
-	bins<-seq(min(Datos_rendimientos[,i]),max(Datos_rendimientos[,i]),
-along.with=(1:particiones))
-
-	histograma<-hist(Datos_rendimientos[,i],main=sprintf("Valor en riesgo de %s", symbols[i]),
-breaks=bins,xlab="rendimientos",ylab="frecuencia",col="blue",border="blue")
-
-dev.off()
-
-histograma$count
-
-
-no_datos<-length(Datos_rendimientos[,i])
-
-prob_acumulada<-cumsum(histograma$count)/no_datos
-
-posicion0<-max(which(prob_acumulada<x))
-
-x0<-prob_acumulada[posicion0]
-x1<-prob_acumulada[posicion0+1]
-y0<-histograma$breaks[posicion0]
-y1<-histograma$breaks[posicion0+1]
-
-y<-y0+((y1-y0)/(x1-x0))*(x-x0)
-
-posicion1<-max(which(prob_acumulada<xa))
-
-x0<-prob_acumulada[posicion1]
-x1<-prob_acumulada[posicion1+1]
-y02<-histograma$breaks[posicion1]
-y12<-histograma$breaks[posicion1+1]
-
-y2<-y02+((y12-y02)/(x1-x0))*(xa-x0)
-
-Nx1<-sum(histograma$count[prob_acumulada<x])
-Nx2<-sum(histograma$count[prob_acumulada<xa])
-datos<-(c(symbols[i],x,y0,y,y1,Nx1,xa,y02,y2,y12,Nx2))
-matriz<-rbind(tabla,datos)
-
+calculo_var <- function(df=Datos_rendimientos,precios = Datos,acciones=100,year){
+  df %<>% filter(`Año`==year) 
+  precios %<>% filter(`Año`==year) 
+  S <- precios[1,1]*acciones
+  media <- df %>% select(-c(`Año`,Fecha)) %>% colMeans()
+  test <- df %>% select(-c(`Año`,Fecha)) %>% var()
+  w <- rep(1,ncol(test))/ncol(test) %>% matrix(nrow = 1,ncol = ncol(test))
+  wt <- w %>% t
+  media_portafolio <- (w%*%media)[1,1]
+  desv <- (w%*%test%*%wt %>% sqrt())[1,1]
+  t <- nrow(df)
+  VaR <- qnorm(0.95,media,desv)*S*desv*sqrt(t)
+  return(VaR)
 }
 
-
-
-names(matriz)<-(c("Variable","x","y0","VAR","y1","CVAR","x","y02","VAR","y12","CVAR"))
-
-View(matriz)
-
+#Hacer for para calcular var cada año y los histogramas de cada 
+calculo_var(year = '2019')
 write.csv(tabla, file = "C:/Users/bluem/OneDrive/Documentos/An?lisis N?merico/TABLA_TAREA1.csv")
 
